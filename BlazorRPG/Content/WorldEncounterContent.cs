a91fd20be0a56ec41eb275a11cfd8675db04fc7f
meatz89 <n.nixdorf@googlemail.com>
2025-04-16 07:34:25 +0200

    update

--- BlazorRPG/Content/WorldEncounterContent.cs
+++ BlazorRPG/Content/WorldEncounterContent.cs
@@ -1,5 +1,33 @@
 ﻿public class WorldEncounterContent
 {
+    public static EncounterTemplate GetDefaultTemplate()
+    {
+        return  new EncounterTemplate()
+            {
+                Name = EncounterNames.ForageForFood.ToString(),
+                Duration = 4,
+                MaxPressure = 12,
+                PartialThreshold = 8,  
+                StandardThreshold = 12, 
+                ExceptionalThreshold = 16, 
+                Hostility = EncounterInfo.HostilityLevels.Neutral,
+
+                EncounterNarrativeTags = new List<NarrativeTag>
+        {
+            NarrativeTagRepository.DistractingCommotion,
+            NarrativeTagRepository.UnsteadyConditions
+        },
+
+                encounterStrategicTags = new List<EnvironmentPropertyTag>
+        {
+            new EnvironmentPropertyTag("Forest Light", Illumination.Shadowy),
+            new EnvironmentPropertyTag("Wildlife Presence", Population.Quiet),
+            new EnvironmentPropertyTag("Uneven Ground", Physical.Hazardous)
+        }
+            };
+}
+
+
     public static List<EncounterTemplate> GetAllTemplates()
     {
         List<EncounterTemplate> encounterTemplates = new()
@@ -43,9 +71,9 @@
         Name = EncounterNames.NightWatch.ToString(),
         Duration = 4,
         MaxPressure = 12,
-        PartialThreshold = 8,   // 1 Food
-        StandardThreshold = 12, // 3 Food
-        ExceptionalThreshold = 16, // 6 Food
+        PartialThreshold = 8,  
+        StandardThreshold = 12, 
+        ExceptionalThreshold = 16, 
         Hostility = EncounterInfo.HostilityLevels.Neutral,
 
         EncounterNarrativeTags = new List<NarrativeTag>
