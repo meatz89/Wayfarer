179ecb009f0427ab2136f2568a84ec7fc91c996b
meatz89 <n.nixdorf@googlemail.com>
2025-04-20 18:30:11 +0200

    update

---
+++ BlazorRPG/GameState/ActionExecutionType.cs
@@ -0,0 +1,5 @@
+﻿public enum ActionExecutionType
+{
+    Basic,          // Immediate effect, no encounter, repeatable
+    Encounter,      // Multi-turn strategic challenge, non-repeatable once completed
+}
