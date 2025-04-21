fa4404dcc8290c2c4a95370ebdc38b4bc2ffbe21
meatz89 <n.nixdorf@googlemail.com>
2025-04-21 11:25:57 +0200

    update

--- BlazorRPG/Pages/EncounterChoiceTooltip.razor.cs
+++ BlazorRPG/Pages/EncounterChoiceTooltip.razor.cs
@@ -5,8 +5,8 @@ public partial class EncounterChoiceTooltipBase : ComponentBase
     [Inject] public GameManager GameManager { get; set; }
     [Inject] public GameState GameState { get; set; }
     [Parameter] public UserEncounterChoiceOption hoveredChoice { get; set; }
-    [Parameter] public double mouseX { get; set; }
-    [Parameter] public double mouseY { get; set; }
+    [Parameter] public double tooltipX { get; set; }
+    [Parameter] public double tooltipY { get; set; }
 
     public ChoiceProjection Preview => GameManager.GetChoicePreview(hoveredChoice);
 
