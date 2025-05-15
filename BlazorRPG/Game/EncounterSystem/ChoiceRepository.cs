801cca505d5caff3c064518b76a6ca5c710cee26
meatz89 <n.nixdorf@googlemail.com>
2025-05-15 17:51:42 +0200

    update

--- BlazorRPG/Game/EncounterSystem/ChoiceRepository.cs
+++ BlazorRPG/Game/EncounterSystem/ChoiceRepository.cs
@@ -14,6 +14,49 @@ public class ChoiceRepository
 
     private void InitializeChoices()
     {
+        cards.Add(CardDefinitionFactory.BuildCard(
+            "imposing_stance",
+            "Imposing Stance",
+            "You adopt a powerful stance, projecting physical dominance and control.",
+            CardTypes.Physical,
+            Skills.Strength,
+            1,
+            1,
+            new List<string>() { "Physical", "Stance" }
+        ));
+
+        cards.Add(CardDefinitionFactory.BuildCard(
+            "intimidate",
+            "Intimidate",
+            "You intimidate your opponent, causing them to hesitate.",
+            CardTypes.Physical,
+            Skills.Strength,
+            1,
+            1,
+            new List<string>() { "Physical", "Intimidate" }
+        ));
+
+        cards.Add(CardDefinitionFactory.BuildCard(
+            "taunt",
+            "Taunt",
+            "You taunt your opponent, drawing their attention and ire.",
+            CardTypes.Physical,
+            Skills.Strength,
+            1,
+            1,
+            new List<string>() { "Physical", "Taunt" }
+        ));
+
+        cards.Add(CardDefinitionFactory.BuildCard(
+            "distract",
+            "Distract",
+            "You distract your opponent, causing them to lose focus.",
+            CardTypes.Physical,
+            Skills.Strength,
+            1,
+            1,
+            new List<string>() { "Physical", "Distract" }
+        ));
     }
 
     public List<CardDefinition> GetAll()
